a74b936d3055abf6c85eb7105664f547bd4638ed
Rafi Nazhmi Nugraha <rafinazhminugraha@upi.edu>
2025-11-30 13:46:27 +0700

    add: separator line aniamation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,18 @@ import WhatIDo from '@/components/WhatIDo';
 import Services from '@/components/Services';
 import Footer from '@/components/Footer';
 
+import { motion } from 'framer-motion';
+
 const Separator = () => (
   <div className="w-full pl-12">
-    <div className="h-0.5 bg-[#1a1a1a] w-full" />
+    <motion.div 
+      initial={{ scaleX: 0 }}
+      whileInView={{ scaleX: 1 }}
+      viewport={{ amount: 0.3 }}
+      transition={{ duration: 1.3, ease: "circOut" }}
+      style={{ originX: 1 }}
+      className="h-0.5 bg-[#1a1a1a] w-full" 
+    />
   </div>
 );
 
